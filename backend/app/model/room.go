2a9e0d1e5e17cea484f4c4a3faeedc7cefe89c0a
koki noguchi <koki.noguchi0202@gmail.com>
2025-04-29 09:33:16 +0900

    roomの作成・メッセージ送信実装

--- backend/app/model/room.go
+++ backend/app/model/room.go
@@ -1,7 +1,34 @@
 package model
 
+import "sync"
+
 type Room struct {
 	name      string
 	broadcast chan []byte
 	clients   map[*Client]bool
+	mu        sync.RWMutex
+}
+
+func NewRoom(roomName string) *Room {
+	return &Room{
+		name:      roomName,
+		broadcast: make(chan []byte),
+		clients:   make(map[*Client]bool),
+	}
+}
+
+func (r *Room) Start() {
+	for {
+		msg := <-r.broadcast
+		r.mu.Lock()
+		for client := range r.clients {
+			select {
+			case client.send <- msg:
+			default:
+				delete(r.clients, client)
+				close(client.send)
+			}
+		}
+		r.mu.Unlock()
+	}
 }
